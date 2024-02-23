77236052fca2815f64b53a1c82c2a197bd475df5
Dmitriy <ka1deo@mail.ru>
2024-02-23 17:07:25 +0300

    fix. import data logic [management]

--- backend/recipes/management/commands/import_data.py
+++ backend/recipes/management/commands/import_data.py
@@ -1,13 +1,32 @@
-import os.path
+import csv
+import os
 
-from django.core.management import BaseCommand
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
 
-from foodgram.settings import BASE_DIR
+
+def ingredient_create(row):
+    Ingredient.objects.get_or_create(
+        name=row[0],
+        measurement_unit=row[1],
+    )
+
+
+action = {
+    "ingredients.csv": ingredient_create,
+}
 
 
 class Command(BaseCommand):
-    help = 'Import .csv to database'
+    def add_arguments(self, parser):
+        parser.add_argument("filename", nargs="+", type=str)
 
     def handle(self, *args, **options):
-        with open(os.path.join(BASE_DIR.parent, 'data/ingredient.json')) as f:
-            pass
+        for filename in options["filename"]:
+            path = os.path.join(settings.BASE_DIR, "data/") + filename
+            with open(path, "r", encoding="utf-8") as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    action[filename](row)
