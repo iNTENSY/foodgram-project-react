7bf629352f6a0f55fc0193f73c651e098dd4cd88
Dmitriy <ka1deo@mail.ru>
2024-03-05 02:46:13 +0300

    minor fixes

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -12,10 +12,10 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.AllValuesMultipleFilter(
+    tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
-        queryset=Tag.objects.all()
+        queryset=Tag.objects.all(),
     )
     is_favorited = filters.BooleanFilter(
         method='get_is_favorited'
