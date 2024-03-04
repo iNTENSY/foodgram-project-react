8eb0cd568e0637c5443a12f488a7b8ea6b7f0a1a
Dmitriy <ka1deo@mail.ru>
2024-03-05 02:34:04 +0300

    fix filters

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,6 +1,6 @@
 from django_filters import filters
 from django_filters.rest_framework import FilterSet
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
 
 
 class IngredientFilter(FilterSet):
@@ -13,9 +13,31 @@ class IngredientFilter(FilterSet):
 
 class RecipeFilter(FilterSet):
     tags = filters.AllValuesMultipleFilter(
-        field_name='tags__slug'
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
+    is_favorited = filters.BooleanFilter(
+        method='get_is_favorited'
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_is_in_shopping_cart'
     )
 
     class Meta:
         model = Recipe
         fields = ('tags', 'author',)
+
+    def get_is_favorited(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(
+                favorites__author=self.request.user,
+            )
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(
+                shopping_list__author=self.request.user,
+            )
+        return queryset
